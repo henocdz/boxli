bb51619061d2e08342d915f84a2a743840556c19
henocdz <henocdz@gmail.com>
2017-04-24 07:26:07 -0500

    Change API host on React

--- front/app/env.js
+++ front/app/env.js
@@ -1,6 +1,6 @@
 const env = {
-  API_BASE_URL: process.env.NODE_ENV === 'production' ? 'http://boxli.co' : 'http://localhost:8000',
-  API_URL: process.env.NODE_ENV === 'production' ? 'http://boxli.co/api' : 'http://localhost:8000/api',
+  API_BASE_URL: process.env.NODE_ENV === 'production' ? 'http://api.boxli.co' : 'http://localhost:8000',
+  API_URL: process.env.NODE_ENV === 'production' ? 'http://api.boxli.co/api' : 'http://localhost:8000/api',
   CREATE_LINK_URL: '/links/'
 }
 
