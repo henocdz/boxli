a0462be89907e0d25362ffac3d9ab556bfbcfd79
henocdz <henocdz@gmail.com>
2017-04-24 12:25:28 -0500

    Ignore case

--- back/boxli/links/views.py
+++ back/boxli/links/views.py
@@ -6,7 +6,6 @@ from links.models import Link
 # Create your views here.
 class RedirectLinkView(View):
     def get(self, request, key, *args, **kwargs):
-        key = key.lower()
         try:
             link = Link.objects.get(key=key)
             link.visits += 1
