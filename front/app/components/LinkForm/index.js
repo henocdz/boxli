a8f9619f7befac844d7d95a8785c96964cf7a0c7
henocdz <henocdz@gmail.com>
2017-04-24 15:11:51 -0500

    Add validation

--- front/app/components/LinkForm/index.js
+++ front/app/components/LinkForm/index.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { findDOMNode } from 'react'
 import BaseComponent from '../BaseComponent'
+
+// import validation from 'react-validation-mixin'
+// import strategy from 'react-validatorjs-strategy'
+
+import Validator from 'validatorjs'
 
 import request from '../../utils/request'
 import ENV from '../../env'
@@ -10,13 +15,16 @@ export default class LinkForm extends BaseComponent {
   constructor(props) {
     super(props)
     this.state = {
-      link: ''
+      link: '',
+      errors: {}
     }
-    this._bind('_onSubmit', '_onChange')
+    this.rules = {
+      link: 'required|url'
+    }
+    this._bind('_onSubmit', '_onChange', '_submit', '_validate', '_renderError')
   }
 
-  _onSubmit(e) {
-    e.preventDefault()
+  _submit() {
     const url = ENV.API_URL + ENV.CREATE_LINK_URL
     request(url, {
       method: 'POST',
@@ -25,9 +33,33 @@ export default class LinkForm extends BaseComponent {
       }
     })
     .then((response) => {
-      this.setState({ link: response.short_url })
+      this.setState({ link: response.short_url, errors: {} })
     })
     .catch(err => err.response.json().then(console.log))
+  }
+
+  _validate(success) {
+    const validator = new Validator(this.state, this.rules)
+    const isValid = validator.passes()
+    if (!isValid) {
+      this.setState({ errors: { ...validator.errors.errors } })
+      return
+    } else if (typeof success === 'function') {
+      success()
+    }
+  }
+
+  _onSubmit(e) {
+    e.preventDefault()
+    this._validate(this._submit)
+  }
+
+  _renderError(field) {
+    if (field in this.state.errors) {
+      const errorsTxt = this.state.errors[field].join(' ')
+      return <p>{errorsTxt}</p>
+    }
+    return null
   }
 
   _onChange(e) {
@@ -39,8 +71,18 @@ export default class LinkForm extends BaseComponent {
   render() {
     return (
       <form onSubmit={this._onSubmit} className="link-form">
-        <input value={this.state.link} onChange={this._onChange} className="input" placeholder="Ingresa una URL" />
-        <button>Crear</button>
+        <div className="input-wrapper">
+          <input
+            value={this.state.link}
+            onChange={this._onChange}
+            className="input"
+            ref="link"
+            placeholder="Ingresa una URL"
+            onBlur={this._validate}
+          />
+          <button>Crear</button>
+        </div>
+        {this._renderError('link')}
       </form>
     )
   }
